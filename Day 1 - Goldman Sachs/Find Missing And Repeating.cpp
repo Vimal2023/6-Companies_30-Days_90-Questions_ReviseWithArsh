8ff9379d020d01f04c67fe526d0c4835fdbeb2e5
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-01-03 00:27:56 +0530

    day2
    Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2,....,N} is missing and one number 'B' occurs twice in array. Find these two numbers.

---
+++ Day 1 - Goldman Sachs/Find Missing And Repeating.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        int i = 0;
+        while (i < n) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr[i], arr[arr[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return {arr[i], i + 1};
+            }
+        }
+        
+        return {};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
